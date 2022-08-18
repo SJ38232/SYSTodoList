641ff7c4357ebfbe188428cefcec34a4818505b1
SJ38232 <tjrwn867@gmail.com>
2022-08-18 11:00:12 +0900

    delete multiple folder

--- public/script.js
+++ public/script.js
@@ -2,17 +2,6 @@ function submitTodo(){
 	var todoListDiv = document.getElementById("todoListDiv");
 	var inputTodo = document.getElementById("inputTodo").value;
 	var inputDeadLine = document.getElementById("inputDeadLine").value;
-	var div = document.createElement("div");
-	var table = document.createElement("table");
-	var statusTr = document.createElement("tr");
-	var contentTr = document.createElement("tr");
-	var checkBoxTd = document.createElement("td");
-	var submitDateTd = document.createElement("td");
-	var deadLineTd = document.createElement("td");
-	var contentTd = document.createElement("td");
-	var deleteButtonTd = document.createElement("td");
-	var checkbox = document.createElement("input");
-	var deleteButton = document.createElement("input");
 	if(inputDeadLine == ""){
 		alert("마감일을 입력해주세요");
 		return;
@@ -21,85 +10,98 @@ function submitTodo(){
 		alert("내용을 입력해주세요");
 		return;
 	}
-	checkbox.setAttribute("type","checkbox");
-	checkbox.setAttribute("onclick","completeCheck(this)");
-	deleteButton.setAttribute("type","button");
-	deleteButton.setAttribute("value","삭제");
-	deleteButton.setAttribute("onclick","deleteTodo(this)");
-	table.setAttribute("class","todo");
-	checkBoxTd.setAttribute("rowspan","2");
-	checkBoxTd.setAttribute("class","checkboxTd");
-	deleteButtonTd.setAttribute("rowspan","2");
-	deleteButtonTd.setAttribute("class","deleteBottonTd");
-	table.appendChild(statusTr);
-	statusTr.appendChild(checkBoxTd);
-	checkBoxTd.appendChild(checkbox);
-	statusTr.append(submitDateTd);
-	statusTr.append(deadLineTd);
-	submitDateTd.setAttribute("class","dateTd");
-	submitDateTd.innerText = "등록일 : "+transDate(new Date())
-	deadLineTd.setAttribute("class","dateTd");
-	deadLineTd.innerText = "마감일 : "+ inputDeadLine;
-	statusTr.appendChild(deleteButtonTd);
-	deleteButtonTd.appendChild(deleteButton);
-	table.appendChild(contentTr);
-	contentTr.append(contentTd);
-	contentTd.setAttribute("class","contentTd");
-	contentTd.setAttribute("colspan","2");
-	contentTd.setAttribute("onclick","contentUpdateMode(this)");
-	contentTd.innerText = inputTodo;
-	div.appendChild(table);
-	todoListDiv.insertBefore(div,todoListDiv.secondChild);
-	
+	createContent(todoListDiv,transDate(new Date),inputDeadLine,inputTodo,false);
 	if(localStorage.getItem("todoList") == null){
-		var saveData = transDate(new Date()) +"/" + inputDeadLine +"/" + inputTodo;
-		localStorage.setItem("todoList",saveData);
+		var saveData = [];
+		saveData.push(transDate(new Date()) +"/" + inputDeadLine +"/" + inputTodo +"/"+"false");
+		localStorage.setItem("todoList",JSON.stringify(saveData));
 	}else{
-		var saveData = ","+ transDate(new Date()) +"/" + inputDeadLine +"/" + inputTodo;
-		loadData = localStorage.getItem("todoList");
-		saveData = loadData + saveData;
-		saveData = saveData.slice(0, -1);
-		localStorage.setItem("todoList",saveData);
+		var saveData = transDate(new Date()) +"/" + inputDeadLine +"/" + inputTodo+"/"+"false";
+		loadData = [];
+		loadData = JSON.parse(localStorage.getItem("todoList"));
+		loadData.push(saveData);
+		localStorage.setItem("todoList",JSON.stringify(loadData));
 	}
 }
-function completeCheck(check){
-	if(check.checked){
-		var table = check.parentElement.parentElement.parentElement;
+function createTodo(todoList){
+	var sapList = todoList.split("/");
+	var todoListDiv = document.getElementById("todoListDiv");
+	var submitDate = sapList[0];
+	var inputTodo = sapList[2];
+	var deadLine = sapList[1];
+	var checked = JSON.parse(sapList[3]);
+	createContent(todoListDiv,submitDate,deadLine,inputTodo,checked);
+}
+function createExpired(expiredList){
+	var sapList = expiredList.split("/");
+	var todoListDiv = document.getElementById("expiredListDiv");
+	var submitDate = sapList[0];
+	var inputTodo = sapList[2];
+	var deadLine = sapList[1];
+	var checked = JSON.parse(sapList[3]);
+	createContent(todoListDiv,submitDate,deadLine,inputTodo,checked);
+}
+function changeCheck(check){
+	var table = check.parentElement.parentElement.parentElement;
+	var dateTr = table.rows[0];
+	var contentTr = table.rows[1];
+	var check = table.rows[0].cells[0].firstChild.checked;
+	var submitDate = dateTr.cells[1].innerText;
+	submitDate = submitDate.substring(6);
+	var deadLine = dateTr.cells[2].innerText;
+	deadLine = deadLine.substring(6);
+	var content = contentTr.firstChild.innerText;
+	var data = submitDate+"/"+deadLine+"/"+content;
+	var todoList = JSON.parse(localStorage.getItem("todoList"));
+	for(i = 0 ; i < todoList.length; i++){
+		if(todoList[i].includes(data)){
+			temp = todoList[i].split("/");
+			todoList[i] = temp[0]+"/"+temp[1]+"/"+temp[2]+"/"+check;
+		}
+	}
+	localStorage.setItem("todoList",JSON.stringify(todoList));
+	if(check){
 		table.setAttribute("class","done");
 	}else{
-		var table = check.parentElement.parentElement.parentElement;
 		table.setAttribute("class","todo");
 	}
 }
-function transDate(date){
-	var day = date.getFullYear()+"-"+ (date.getMonth()+1)+"-"+date.getDate();
-	return day;
-}
+
 function deleteTodo(button){
 	var div = button.parentElement.parentElement.parentElement.parentElement;
+	var table = button.parentElement.parentElement.parentElement;
+	var dateTr = table.rows[0];
+	var contentTr = table.rows[1];
+	var check = table.rows[0].cells[0].firstChild.checked;
+	var submitDate = dateTr.cells[1].innerText;
+	submitDate = submitDate.substring(6);
+	var deadLine = dateTr.cells[2].innerText;
+	deadLine = deadLine.substring(6);
+	var content = contentTr.firstChild.innerText;
+	var data = submitDate+"/"+deadLine+"/"+content+"/"+check;
+	var todoList = JSON.parse(localStorage.getItem("todoList"));
+	for(i = 0 ; i < todoList.length; i++){
+		if(data == todoList[i]){
+			todoList.splice(i,1);
+		}
+	}
+	localStorage.setItem("todoList",JSON.stringify(todoList));
+	
 	div.remove();
-}
-function parseLocalStorage(){
-	var loadData = localStorage.getItem("todoList");
-	var todoList = loadData.split(",");
-	return todoList;
 }
 function checkExpired(todoList){
 	var sapList = todoList.split("/");
 	var sapDeadLine = sapList[1].split("-");
 	var deadLine = new Date(sapDeadLine[0],sapDeadLine[1]-1,sapDeadLine[2]);
 	var today = new Date();
+	today.setHours(0,0,0,0);
 	if(deadLine < today){
 		return true;
 	}else{
 		return false;
 	}
 }
-function createTodo(todoList){
-	var sapList = todoList.split("/");
-	var todoListDiv = document.getElementById("todoListDiv");
-	var inputTodo = sapList[2];
-	var inputDeadLine = sapList[1];
+function createContent(targetDiv,submitDate,deadline,content,checked){
 	var div = document.createElement("div");
 	var table = document.createElement("table");
 	var statusTr = document.createElement("tr");
@@ -111,91 +113,68 @@ function createTodo(todoList){
 	var deleteButtonTd = document.createElement("td");
 	var checkbox = document.createElement("input");
 	var deleteButton = document.createElement("input");
+	table.appendChild(statusTr);
+	statusTr.appendChild(checkBoxTd);
+	checkBoxTd.appendChild(checkbox);
+	statusTr.append(submitDateTd);
+	statusTr.append(deadLineTd);
+	statusTr.appendChild(deleteButtonTd);
+	deleteButtonTd.appendChild(deleteButton);
+	contentTr.append(contentTd);
+	table.appendChild(contentTr);
+	div.appendChild(table);
 	checkbox.setAttribute("type","checkbox");
-	checkbox.setAttribute("onclick","completeCheck(this)");
+	checkbox.setAttribute("onclick","changeCheck(this)");
+	checkbox.checked = checked;
 	deleteButton.setAttribute("type","button");
 	deleteButton.setAttribute("value","삭제");
 	deleteButton.setAttribute("onclick","deleteTodo(this)");
-	table.setAttribute("class","todo");
 	checkBoxTd.setAttribute("rowspan","2");
 	checkBoxTd.setAttribute("class","checkboxTd");
 	deleteButtonTd.setAttribute("rowspan","2");
 	deleteButtonTd.setAttribute("class","deleteBottonTd");
-	table.appendChild(statusTr);
-	statusTr.appendChild(checkBoxTd);
-	checkBoxTd.appendChild(checkbox);
-	statusTr.append(submitDateTd);
-	statusTr.append(deadLineTd);
 	submitDateTd.setAttribute("class","dateTd");
-	submitDateTd.innerText = "등록일 : "+ sapList[0];
+	submitDateTd.innerText = "등록일 : "+submitDate;
 	deadLineTd.setAttribute("class","dateTd");
-	deadLineTd.innerText = "마감일 : "+ inputDeadLine;
-	statusTr.appendChild(deleteButtonTd);
-	deleteButtonTd.appendChild(deleteButton);
-	table.appendChild(contentTr);
-	contentTr.append(contentTd);
+	deadLineTd.innerText = "마감일 : "+ deadline;
 	contentTd.setAttribute("class","contentTd");
 	contentTd.setAttribute("colspan","2");
 	contentTd.setAttribute("onclick","contentUpdateMode(this)");
-	contentTd.innerText = inputTodo;
-	div.appendChild(table);
-	todoListDiv.insertBefore(div,todoListDiv.secondChild);
+	contentTd.innerText = content;
+	if(targetDiv.getAttribute("id") == "expiredListDiv"){
+		if(checkbox.checked){
+			deleteTodo(deleteButton);
+			return;
+		} else {
+			table.setAttribute("class","expired");
+			checkbox.setAttribute("onclick","");
+		}
+	}else{
+		if(checkbox.checked){
+			table.setAttribute("class","done");
+		}else{
+			table.setAttribute("class","todo");
+		}
+	}
+	targetDiv.insertBefore(div,targetDiv.secondChild);
+	document.getElementById("inputTodo").value = "";
 }
-function createExpired(expiredList){
-	var sapList = expiredList.split("/");
-	var todoListDiv = document.getElementById("expiredListDiv");
-	var inputTodo = sapList[2];
-	var inputDeadLine = sapList[1];
-	var div = document.createElement("div");
-	var table = document.createElement("table");
-	var statusTr = document.createElement("tr");
-	var contentTr = document.createElement("tr");
-	var checkBoxTd = document.createElement("td");
-	var submitDateTd = document.createElement("td");
-	var deadLineTd = document.createElement("td");
-	var contentTd = document.createElement("td");
-	var deleteButtonTd = document.createElement("td");
-	var checkbox = document.createElement("input");
-	var deleteButton = document.createElement("input");
-	checkbox.setAttribute("type","checkbox");
-	checkbox.setAttribute("onclick","completeCheck(this)");
-	deleteButton.setAttribute("type","button");
-	deleteButton.setAttribute("value","삭제");
-	deleteButton.setAttribute("onclick","deleteTodo(this)");
-	table.setAttribute("class","expired");
-	checkBoxTd.setAttribute("rowspan","2");
-	checkBoxTd.setAttribute("class","expiredcheckboxTd");
-	deleteButtonTd.setAttribute("rowspan","2");
-	deleteButtonTd.setAttribute("class","expireddeleteBottonTd");
-	table.appendChild(statusTr);
-	statusTr.appendChild(checkBoxTd);
-	checkBoxTd.appendChild(checkbox);
-	statusTr.append(submitDateTd);
-	statusTr.append(deadLineTd);
-	submitDateTd.setAttribute("class","expireddateTd");
-	submitDateTd.innerText = "등록일 : "+ sapList[0];
-	deadLineTd.setAttribute("class","expireddateTd");
-	deadLineTd.innerText = "마감일 : "+ inputDeadLine;
-	statusTr.appendChild(deleteButtonTd);
-	deleteButtonTd.appendChild(deleteButton);
-	table.appendChild(contentTr);
-	contentTr.append(contentTd);
-	contentTd.setAttribute("class","expiredcontentTd");
-	contentTd.setAttribute("colspan","2");
-	contentTd.setAttribute("onclick","contentUpdateMode(this)");
-	contentTd.innerText = inputTodo;
-	div.appendChild(table);
-	todoListDiv.insertBefore(div,todoListDiv.secondChild);
+function transDate(date){
+	var day = date.getFullYear()+"-"+ (date.getMonth()+1)+"-"+date.getDate();
+	return day;
 }
 window.onload = function(){
 	var today = document.getElementById("today");
 	today.innerText = "등록일 : "+transDate(new Date()); 
-  	var rawList = parseLocalStorage();
-  	for(i = 0; i < rawList.length;i++){
-		if(checkExpired(rawList[i])){
-			createExpired(rawList[i]);
+	var todoList = [];
+  	if(localStorage.getItem("todoList") !=null || localStorage.getItem("todoList") == ""){
+		todoList = JSON.parse(localStorage.getItem("todoList"));
+	}
+  	for(i = 0; i < todoList.length;i++){
+		if(checkExpired(todoList[i])){
+			createExpired(todoList[i]);
 		}else{
-			createTodo(rawList[i]);
+			createTodo(todoList[i]);
 		}
 	}
 }
