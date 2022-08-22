5154a4b3ea49e61a53534bf7c4b5ceaeb02aacc6
SJ38232 <tjrwn867@gmail.com>
2022-08-22 11:51:02 +0900

    add comment

--- public/script.js
+++ public/script.js
@@ -1,4 +1,6 @@
-function submitTodo() {
+//HTML에서 Data를 입력받아 새로운 Content 생성
+function submitContent() {
+    // HTML에서 입력을 받고 조건을 만족하지 못할경우 alert
     let todoListDiv = document.getElementById("todoListDiv");
     let inputTodo = document.getElementById("inputTodo").value;
     let inputDeadLine = document.getElementById("inputDeadLine").value;
@@ -11,12 +13,12 @@ function submitTodo() {
         return;
     }
     createContent(todoListDiv, transDate(new Date), inputDeadLine, inputTodo, false);
-    if (localStorage.getItem("todoList") == null) {
+    if (localStorage.getItem("todoList") == null) { // localStorage가 없을 경우 새로 만듬(등록시간/만료시간/내용/체크여부)순서의 string 데이터
         let saveData = [];
         saveData.push(transDate(new Date()) + "/" + inputDeadLine + "/" + inputTodo + "/" + "false");
         localStorage.setItem("todoList", JSON.stringify(saveData));
     }
-    else {
+    else { // localStorage가 있을 경우 기존의 localStorage의 뒤에 붙임 
         let saveData = transDate(new Date()) + "/" + inputDeadLine + "/" + inputTodo + "/" + "false";
         let loadData = [];
         loadData = JSON.parse(localStorage.getItem("todoList"));
@@ -24,43 +26,48 @@ function submitTodo() {
         localStorage.setItem("todoList", JSON.stringify(loadData));
     }
 }
+//todoList에 들어갈 Content를 만듬
 function createTodo(todoContent) {
-    let sapList = todoContent.split("/");
+    let sapList = todoContent.split("/"); //(등록시간/만료시간/내용/체크여부)형식을 자름
     let todoListDiv = document.getElementById("todoListDiv");
     let submitDate = sapList[0];
-    let inputTodo = sapList[2];
     let deadLine = sapList[1];
+    let inputTodo = sapList[2];
     let checked = JSON.parse(sapList[3]);
     createContent(todoListDiv, submitDate, deadLine, inputTodo, checked);
 }
+//ExpiredList에 들어갈 Content를 만듬
 function createExpired(expiredContent) {
-    let sapList = expiredContent.split("/");
+    let sapList = expiredContent.split("/"); //(등록시간/만료시간/내용/체크여부)형식을 자름
     let todoListDiv = document.getElementById("expiredListDiv");
     let submitDate = sapList[0];
-    let inputTodo = sapList[2];
     let deadLine = sapList[1];
+    let inputTodo = sapList[2];
     let checked = JSON.parse(sapList[3]);
     createContent(todoListDiv, submitDate, deadLine, inputTodo, checked);
 }
+//Content의 Check의 onclick 함수
 function changeCheck(check) {
+    //check를 기준으로 포함된 테이블 정보 읽음
     let table = check.parentElement.parentElement.parentElement;
     let dateTr = table.rows[0];
     let contentTr = table.rows[1];
     let checked = table.rows[0].cells[0].firstChild.checked;
     let submitDate = dateTr.cells[1].innerText;
     let deadLine = dateTr.cells[2].innerText;
-    submitDate = submitDate.substring(6);
-    deadLine = deadLine.substring(6);
+    submitDate = submitDate.substring(6); //(등록일 : )제거
+    deadLine = deadLine.substring(6); //(마감일 : )제거
     let content = contentTr.cells[0].innerText;
     let data = submitDate + "/" + deadLine + "/" + content;
     let todoList = JSON.parse(localStorage.getItem("todoList"));
     for (let i = 0; i < todoList.length; i++) {
-        if (todoList[i].includes(data)) {
+        if (todoList[i].includes(data)) { //localStorage에 저장된 데이터 변경
             let temp = todoList[i].split("/");
             todoList[i] = temp[0] + "/" + temp[1] + "/" + temp[2] + "/" + checked;
         }
     }
-    localStorage.setItem("todoList", JSON.stringify(todoList));
+    localStorage.setItem("todoList", JSON.stringify(todoList)); //localStorage에 변경사항 적용
+    //check타입에 따라 css 스타일 변경
     if (checked) {
         table.setAttribute("class", "done");
     }
@@ -68,7 +75,9 @@ function changeCheck(check) {
         table.setAttribute("class", "todo");
     }
 }
-function deleteTodo(button) {
+//Content를 localStorage와 HTML에서 삭제
+function deleteContent(button) {
+    //button을 기준으로 포함된 테이블 정보 읽음
     let div = button.parentElement.parentElement.parentElement.parentElement;
     let table = button.parentElement.parentElement.parentElement;
     let dateTr = table.rows[0];
@@ -82,19 +91,21 @@ function deleteTodo(button) {
     let data = submitDate + "/" + deadLine + "/" + content + "/" + checked;
     let todoList = JSON.parse(localStorage.getItem("todoList"));
     for (let i = 0; i < todoList.length; i++) {
-        if (data == todoList[i]) {
+        if (data == todoList[i]) { //일치한 string을 제거하여 localStorage에서 해당 Content삭제
             todoList.splice(i, 1);
         }
     }
-    localStorage.setItem("todoList", JSON.stringify(todoList));
-    div.remove();
+    localStorage.setItem("todoList", JSON.stringify(todoList)); //localStorage에 변경사항 적용
+    div.remove(); //div 삭제하여  HTML상의 Content 삭제
 }
+//날짜를 비교하여 만료 여부 확인
 function checkExpired(todoContent) {
-    let sapList = todoContent.split("/");
-    let sapDeadLine = sapList[1].split("-");
+    let sapList = todoContent.split("/"); //(등록시간/만료시간/내용/체크여부)형식을 자름
+    let sapDeadLine = sapList[1].split("-"); //(년-월-일)형식을 자름
+    // 현재 시간과 비교하기 위해 자른 데이터를 바탕으로 Date 타입을 생성
     let deadLine = new Date(parseInt(sapDeadLine[0]), parseInt(sapDeadLine[1]) - 1, parseInt(sapDeadLine[2]));
     let today = new Date();
-    today.setHours(0, 0, 0, 0);
+    today.setHours(0, 0, 0, 0); //시간을 0으로 맞춰 조건을 동등하게 맞춤
     if (deadLine < today) {
         return true;
     }
@@ -102,7 +113,9 @@ function checkExpired(todoContent) {
         return false;
     }
 }
+//매개변수를 이용하여 새로운 Content 제작
 function createContent(targetDiv, submitDate, deadline, content, checked) {
+    //Content의 모양을 잡아주기 위해 HTMLDOM들을 생성하여 부모자식관계 형성
     let div = document.createElement("div");
     let table = document.createElement("table");
     let statusTr = document.createElement("tr");
@@ -124,6 +137,7 @@ function createContent(targetDiv, submitDate, deadline, content, checked) {
     contentTr.append(contentTd);
     table.appendChild(contentTr);
     div.appendChild(table);
+    //매개변수를 이용하여 내용 설정
     checkbox.setAttribute("type", "checkbox");
     checkbox.setAttribute("onclick", "changeCheck(this)");
     checkbox.checked = checked;
@@ -135,7 +149,7 @@ function createContent(targetDiv, submitDate, deadline, content, checked) {
     deadLineTd.innerText = "마감일 : " + deadline;
     deleteButton.setAttribute("type", "button");
     deleteButton.setAttribute("value", "삭제");
-    deleteButton.setAttribute("onclick", "deleteTodo(this)");
+    deleteButton.setAttribute("onclick", "deleteContent(this)");
     deleteButtonTd.setAttribute("rowspan", "2");
     deleteButtonTd.setAttribute("class", "deleteBottonTd");
     contentTd.setAttribute("class", "contentTd");
@@ -143,8 +157,8 @@ function createContent(targetDiv, submitDate, deadline, content, checked) {
     contentTd.setAttribute("onclick", "contentUpdateMode(this)");
     contentTd.innerText = content;
     if (targetDiv.getAttribute("id") == "expiredListDiv") {
-        if (checkbox.checked) {
-            deleteTodo(deleteButton);
+        if (checkbox.checked) { // expiredList에 속하면서 이미 완료된 Content의 경우 표시할 필요가 없으므로 삭제
+            deleteContent(deleteButton);
             return;
         }
         else {
@@ -160,10 +174,14 @@ function createContent(targetDiv, submitDate, deadline, content, checked) {
             table.setAttribute("class", "todo");
         }
     }
-    let insertTarget = targetDiv.childNodes[2];
+    let insertTarget = targetDiv.childNodes[2]; //역순으로 Content생성
     targetDiv.insertBefore(div, insertTarget);
-    document.getElementById("inputTodo").value = "";
+    document.getElementById("inputTodo").value = ""; // 다음 입력을 위하여 input의 스트링 초기화
 }
+//Content 내용 수정
+function updateContent(content) {
+}
+//Date 타입을 String으로 변환
 function transDate(date) {
     let day = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
     return day;
@@ -175,6 +193,7 @@ window.onload = function () {
     if (localStorage.getItem("todoList") != null || localStorage.getItem("todoList") == "") {
         todoList = JSON.parse(localStorage.getItem("todoList"));
     }
+    //만료 여부를 확인하여 todoList와 expiredList 생성
     for (let i = 0; i < todoList.length; i++) {
         if (checkExpired(todoList[i])) {
             createExpired(todoList[i]);
--- public/script.ts
+++ public/script.ts
@@ -1,118 +1,130 @@
-function submitTodo(){
-	var todoListDiv = document.getElementById("todoListDiv");
-	var inputTodo = document.getElementById("inputTodo").value;
-	var inputDeadLine = document.getElementById("inputDeadLine").value;
+//HTML에서 Data를 입력받아 새로운 Content 생성
+function submitContent(){
+	// HTML에서 입력을 받고 조건을 만족하지 못할경우 alert
+	let todoListDiv: HTMLDivElement = <HTMLDivElement>document.getElementById("todoListDiv");
+	let inputTodo: string = (<HTMLInputElement>document.getElementById("inputTodo")).value;
+	let inputDeadLine: string = (<HTMLInputElement>document.getElementById("inputDeadLine")).value;
 	if(inputDeadLine == ""){
 		alert("마감일을 입력해주세요");
 		return;
 	}
-	if(inputTodo.value == ""){
+	if(inputTodo == ""){
 		alert("내용을 입력해주세요");
 		return;
 	}
 	createContent(todoListDiv,transDate(new Date),inputDeadLine,inputTodo,false);
-	if(localStorage.getItem("todoList") == null){
-		var saveData = [];
+	if(localStorage.getItem("todoList") == null){// localStorage가 없을 경우 새로 만듬(등록시간/만료시간/내용/체크여부)순서의 string 데이터
+		let saveData:string[] = [];
 		saveData.push(transDate(new Date()) +"/" + inputDeadLine +"/" + inputTodo +"/"+"false");
 		localStorage.setItem("todoList",JSON.stringify(saveData));
-	}else{
-		var saveData = transDate(new Date()) +"/" + inputDeadLine +"/" + inputTodo+"/"+"false";
-		loadData = [];
+	}else{// localStorage가 있을 경우 기존의 localStorage의 뒤에 붙임 
+		let saveData: string = transDate(new Date()) +"/" + inputDeadLine +"/" + inputTodo+"/"+"false";
+		let loadData: string[] = [];
 		loadData = JSON.parse(localStorage.getItem("todoList"));
 		loadData.push(saveData);
 		localStorage.setItem("todoList",JSON.stringify(loadData));
 	}
 }
-function createTodo(todoList){
-	var sapList = todoList.split("/");
-	var todoListDiv = document.getElementById("todoListDiv");
-	var submitDate = sapList[0];
-	var inputTodo = sapList[2];
-	var deadLine = sapList[1];
-	var checked = JSON.parse(sapList[3]);
+//todoList에 들어갈 Content를 만듬
+function createTodo(todoContent: string){
+	let sapList: string[] = todoContent.split("/");//(등록시간/만료시간/내용/체크여부)형식을 자름
+	let todoListDiv: HTMLDivElement = <HTMLDivElement>document.getElementById("todoListDiv");
+	let submitDate: string = sapList[0];
+	let deadLine: string = sapList[1];
+	let inputTodo: string = sapList[2];
+	let checked: boolean = JSON.parse(sapList[3]);
 	createContent(todoListDiv,submitDate,deadLine,inputTodo,checked);
 }
-function createExpired(expiredList){
-	var sapList = expiredList.split("/");
-	var todoListDiv = document.getElementById("expiredListDiv");
-	var submitDate = sapList[0];
-	var inputTodo = sapList[2];
-	var deadLine = sapList[1];
-	var checked = JSON.parse(sapList[3]);
+//ExpiredList에 들어갈 Content를 만듬
+function createExpired(expiredContent: string){
+	let sapList: string[] = expiredContent.split("/");//(등록시간/만료시간/내용/체크여부)형식을 자름
+	let todoListDiv: HTMLDivElement = <HTMLDivElement>document.getElementById("expiredListDiv");
+	let submitDate: string = sapList[0];
+	let deadLine: string = sapList[1];
+	let inputTodo: string = sapList[2];
+	let checked: boolean = JSON.parse(sapList[3]);
 	createContent(todoListDiv,submitDate,deadLine,inputTodo,checked);
 }
-function changeCheck(check){
-	var table = check.parentElement.parentElement.parentElement;
-	var dateTr = table.rows[0];
-	var contentTr = table.rows[1];
-	var check = table.rows[0].cells[0].firstChild.checked;
-	var submitDate = dateTr.cells[1].innerText;
-	submitDate = submitDate.substring(6);
-	var deadLine = dateTr.cells[2].innerText;
-	deadLine = deadLine.substring(6);
-	var content = contentTr.firstChild.innerText;
-	var data = submitDate+"/"+deadLine+"/"+content;
-	var todoList = JSON.parse(localStorage.getItem("todoList"));
-	for(i = 0 ; i < todoList.length; i++){
-		if(todoList[i].includes(data)){
-			temp = todoList[i].split("/");
-			todoList[i] = temp[0]+"/"+temp[1]+"/"+temp[2]+"/"+check;
+//Content의 Check의 onclick 함수
+function changeCheck(check: HTMLElement){
+	//check를 기준으로 포함된 테이블 정보 읽음
+	let table: HTMLTableElement = <HTMLTableElement>check.parentElement.parentElement.parentElement;
+	let dateTr: HTMLTableRowElement = table.rows[0];
+	let contentTr: HTMLTableRowElement = table.rows[1];
+	let checked: boolean = (<HTMLInputElement>table.rows[0].cells[0].firstChild).checked;
+	let submitDate: string = dateTr.cells[1].innerText;
+	let deadLine: string = dateTr.cells[2].innerText;
+	submitDate = submitDate.substring(6);//(등록일 : )제거
+	deadLine = deadLine.substring(6);//(마감일 : )제거
+	let content: string = contentTr.cells[0].innerText;
+	let data: string = submitDate+"/"+deadLine+"/"+content;
+	let todoList: string[] = JSON.parse(localStorage.getItem("todoList"));
+	for(let i:number = 0 ; i < todoList.length; i++){
+		if(todoList[i].includes(data)){//localStorage에 저장된 데이터 변경
+			let temp: string[] = todoList[i].split("/");
+			todoList[i] = temp[0]+"/"+temp[1]+"/"+temp[2]+"/"+checked;
 		}
 	}
-	localStorage.setItem("todoList",JSON.stringify(todoList));
-	if(check){
+	localStorage.setItem("todoList",JSON.stringify(todoList));//localStorage에 변경사항 적용
+	//check타입에 따라 css 스타일 변경
+	if(checked){
 		table.setAttribute("class","done");
 	}else{
 		table.setAttribute("class","todo");
 	}
 }
-
-function deleteTodo(button){
-	var div = button.parentElement.parentElement.parentElement.parentElement;
-	var table = button.parentElement.parentElement.parentElement;
-	var dateTr = table.rows[0];
-	var contentTr = table.rows[1];
-	var check = table.rows[0].cells[0].firstChild.checked;
-	var submitDate = dateTr.cells[1].innerText;
+//Content를 localStorage와 HTML에서 삭제
+function deleteContent(button: HTMLButtonElement){
+	//button을 기준으로 포함된 테이블 정보 읽음
+	let div: HTMLDivElement = <HTMLDivElement>button.parentElement.parentElement.parentElement.parentElement;
+	let table: HTMLTableElement  = <HTMLTableElement>button.parentElement.parentElement.parentElement;
+	let dateTr: HTMLTableRowElement = table.rows[0];
+	let contentTr: HTMLTableRowElement = table.rows[1];
+	let checked: boolean = (<HTMLInputElement>table.rows[0].cells[0].firstChild).checked;
+	let submitDate: string = dateTr.cells[1].innerText;
+	let deadLine: string = dateTr.cells[2].innerText;
 	submitDate = submitDate.substring(6);
-	var deadLine = dateTr.cells[2].innerText;
 	deadLine = deadLine.substring(6);
-	var content = contentTr.firstChild.innerText;
-	var data = submitDate+"/"+deadLine+"/"+content+"/"+check;
-	var todoList = JSON.parse(localStorage.getItem("todoList"));
-	for(i = 0 ; i < todoList.length; i++){
-		if(data == todoList[i]){
+	let content: string = contentTr.cells[0].innerText;
+	let data: string = submitDate+"/"+deadLine+"/"+content+"/"+checked;
+	let todoList:string[] = JSON.parse(localStorage.getItem("todoList"));
+	for(let i:number = 0 ; i < todoList.length; i++){
+		if(data == todoList[i]){//일치한 string을 제거하여 localStorage에서 해당 Content삭제
 			todoList.splice(i,1);
 		}
 	}
-	localStorage.setItem("todoList",JSON.stringify(todoList));
+	localStorage.setItem("todoList",JSON.stringify(todoList));//localStorage에 변경사항 적용
 	
-	div.remove();
+	div.remove();//div 삭제하여  HTML상의 Content 삭제
 }
-function checkExpired(todoList){
-	var sapList = todoList.split("/");
-	var sapDeadLine = sapList[1].split("-");
-	var deadLine = new Date(sapDeadLine[0],sapDeadLine[1]-1,sapDeadLine[2]);
-	var today = new Date();
-	today.setHours(0,0,0,0);
+//날짜를 비교하여 만료 여부 확인
+function checkExpired(todoContent: string){
+	let sapList: string[] = todoContent.split("/");//(등록시간/만료시간/내용/체크여부)형식을 자름
+	let sapDeadLine: string[] = sapList[1].split("-");//(년-월-일)형식을 자름
+	// 현재 시간과 비교하기 위해 자른 데이터를 바탕으로 Date 타입을 생성
+	let deadLine: Date = new Date(parseInt(sapDeadLine[0]),parseInt(sapDeadLine[1])-1,parseInt(sapDeadLine[2]));
+	let today: Date = new Date();
+	today.setHours(0,0,0,0);//시간을 0으로 맞춰 조건을 동등하게 맞춤
 	if(deadLine < today){
 		return true;
 	}else{
 		return false;
 	}
 }
-function createContent(targetDiv,submitDate,deadline,content,checked){
-	var div = document.createElement("div");
-	var table = document.createElement("table");
-	var statusTr = document.createElement("tr");
-	var contentTr = document.createElement("tr");
-	var checkBoxTd = document.createElement("td");
-	var submitDateTd = document.createElement("td");
-	var deadLineTd = document.createElement("td");
-	var contentTd = document.createElement("td");
-	var deleteButtonTd = document.createElement("td");
-	var checkbox = document.createElement("input");
-	var deleteButton = document.createElement("input");
+//매개변수를 이용하여 새로운 Content 제작
+function createContent(targetDiv: HTMLDivElement,submitDate: string,deadline: string,content: string,checked: boolean){
+	//Content의 모양을 잡아주기 위해 HTMLDOM들을 생성하여 부모자식관계 형성
+	let div: HTMLDivElement = document.createElement("div");
+	let table: HTMLTableElement = document.createElement("table");
+	let statusTr: HTMLTableRowElement = document.createElement("tr");
+	let contentTr: HTMLTableRowElement = document.createElement("tr");
+	let checkBoxTd: HTMLTableCellElement = document.createElement("td");
+	let submitDateTd: HTMLTableCellElement = document.createElement("td");
+	let deadLineTd: HTMLTableCellElement = document.createElement("td");
+	let contentTd: HTMLTableCellElement = document.createElement("td");
+	let deleteButtonTd: HTMLTableCellElement = document.createElement("td");
+	let checkbox: HTMLInputElement = document.createElement("input");
+	let deleteButton: HTMLInputElement = document.createElement("input");
 	table.appendChild(statusTr);
 	statusTr.appendChild(checkBoxTd);
 	checkBoxTd.appendChild(checkbox);
@@ -123,27 +135,28 @@ function createContent(targetDiv,submitDate,deadline,content,checked){
 	contentTr.append(contentTd);
 	table.appendChild(contentTr);
 	div.appendChild(table);
+	//매개변수를 이용하여 내용 설정
 	checkbox.setAttribute("type","checkbox");
 	checkbox.setAttribute("onclick","changeCheck(this)");
 	checkbox.checked = checked;
-	deleteButton.setAttribute("type","button");
-	deleteButton.setAttribute("value","삭제");
-	deleteButton.setAttribute("onclick","deleteTodo(this)");
 	checkBoxTd.setAttribute("rowspan","2");
 	checkBoxTd.setAttribute("class","checkboxTd");
-	deleteButtonTd.setAttribute("rowspan","2");
-	deleteButtonTd.setAttribute("class","deleteBottonTd");
 	submitDateTd.setAttribute("class","dateTd");
 	submitDateTd.innerText = "등록일 : "+submitDate;
 	deadLineTd.setAttribute("class","dateTd");
 	deadLineTd.innerText = "마감일 : "+ deadline;
+	deleteButton.setAttribute("type","button");
+	deleteButton.setAttribute("value","삭제");
+	deleteButton.setAttribute("onclick","deleteContent(this)");
+	deleteButtonTd.setAttribute("rowspan","2");
+	deleteButtonTd.setAttribute("class","deleteBottonTd");
 	contentTd.setAttribute("class","contentTd");
 	contentTd.setAttribute("colspan","2");
 	contentTd.setAttribute("onclick","contentUpdateMode(this)");
 	contentTd.innerText = content;
 	if(targetDiv.getAttribute("id") == "expiredListDiv"){
-		if(checkbox.checked){
-			deleteTodo(deleteButton);
+		if(checkbox.checked){// expiredList에 속하면서 이미 완료된 Content의 경우 표시할 필요가 없으므로 삭제
+			deleteContent(deleteButton);
 			return;
 		} else {
 			table.setAttribute("class","expired");
@@ -156,21 +169,28 @@ function createContent(targetDiv,submitDate,deadline,content,checked){
 			table.setAttribute("class","todo");
 		}
 	}
-	targetDiv.insertBefore(div,targetDiv.secondChild);
-	document.getElementById("inputTodo").value = "";
+	let insertTarget = targetDiv.childNodes[2];//역순으로 Content생성
+	targetDiv.insertBefore(div,insertTarget);
+	(<HTMLInputElement>document.getElementById("inputTodo")).value = "";// 다음 입력을 위하여 input의 스트링 초기화
 }
-function transDate(date){
-	var day = date.getFullYear()+"-"+ (date.getMonth()+1)+"-"+date.getDate();
+//Content 내용 수정
+function updateContent(content: HTMLTableCellElement){
+	
+}
+//Date 타입을 String으로 변환
+function transDate(date: Date){
+	let day: string = date.getFullYear()+"-"+ (date.getMonth()+1)+"-"+date.getDate();
 	return day;
 }
 window.onload = function(){
-	var today = document.getElementById("today");
+	let today: HTMLTableCellElement = <HTMLTableCellElement>document.getElementById("today");
 	today.innerText = "등록일 : "+transDate(new Date()); 
-	var todoList = [];
-  	if(localStorage.getItem("todoList") !=null || localStorage.getItem("todoList") == ""){
+	let todoList: string[] = [];
+  	if(localStorage.getItem("todoList") != null || localStorage.getItem("todoList") == ""){
 		todoList = JSON.parse(localStorage.getItem("todoList"));
 	}
-  	for(i = 0; i < todoList.length;i++){
+	//만료 여부를 확인하여 todoList와 expiredList 생성
+  	for(let i: number = 0; i < todoList.length;i++){
 		if(checkExpired(todoList[i])){
 			createExpired(todoList[i]);
 		}else{
